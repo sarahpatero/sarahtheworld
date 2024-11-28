74327072323205aa26d7af2c4995be5546b35dc5
sarahpatero <spatero@umich.edu>
2024-11-28 13:58:07 -0500

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    if (!document.querySelector("#name").value || !document.querySelector("#email").value || !document.querySelector("#message").value) {
-      alert("Please fill out all fields!");
-      event.preventDefault(); // Prevent form submission
-    }
-  });
-  
